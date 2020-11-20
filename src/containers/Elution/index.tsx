aec71856bca5fddecf448dc5b377736a6c900dc6
CanadaApollo6 <riel.stamand@gmail.com>
2020-11-20 10:13:03 -0500

    Add stats

--- src/containers/Elution/index.tsx
+++ src/containers/Elution/index.tsx
@@ -6,7 +6,6 @@ import { PlateContext } from '../../contexts/PlateContext'
 const Elution: React.FC = () => {
     const { determinations } = useContext(PlateContext)
     const elutions = Array.from(determinations.reduce((acc, { elution }) => acc.add(elution), new Set()))
-
     return (
         <>
             {elutions.map(e => (
@@ -18,7 +17,13 @@ const Elution: React.FC = () => {
                                     <PlateMap samples={determinations.filter(s => s.elution === e)} rowCount={12} />
                                 </Col>
                                 <Col span={16}>
-                                    <p>TODO - Reasons and Statistics</p>
+                                    <h1>Statistcs</h1>
+                                    <h3>Not Detected: {(determinations.filter(d => d.elution === e)).filter(d => d.determination === "Not Detected").length}</h3>
+                                    <h3>Detected: {(determinations.filter(d => d.elution === e)).filter(d => d.determination === "Detected").length}</h3>
+                                    <h3>Repeat: {(determinations.filter(d => d.elution === e)).filter(d => d.determination === "Repeat").length}</h3>
+                                    <h3>Inconclusive: {(determinations.filter(d => d.elution === e)).filter(d => d.determination === "Inconclusive").length - (determinations.filter(d => d.elution === e)).filter(d => d.sample_id === "neg" || d.sample_id === "pc").length}</h3>
+                                    <h3>Invalid: {(determinations.filter(d => d.elution === e)).filter(d => d.determination === "Invalid").length}</h3>
+                                    <h3>Controls: {(determinations.filter(d => d.elution === e)).filter(d => d.sample_id === "neg" || d.sample_id === "pc").length}</h3>
                                 </Col>
                             </Row>
                         </Card>
--- src/containers/Plate/index.tsx
+++ src/containers/Plate/index.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react'
-import { Card, Row, Col } from 'antd'
+import { Card, Row, Col, Statistic } from 'antd'
 import { ContextGraphOptions } from '../../components/GraphOptions'
 import { GraphContextProvider } from '../../contexts/GraphContext'
 import { PlateContext } from '../../contexts/PlateContext'
@@ -7,9 +7,18 @@ import { SampleDetermination } from '../../types'
 import PlateMap from '../../components/PlateMap'
 import { SampleGraph } from '../Rows/RowGraphs'
 
+
 export default () => {
     const { determinations } = useContext(PlateContext)
     const [determination, setDetermination] = useState<SampleDetermination>()
+    const statistics = {
+        notDetected: determinations.filter(d => d.determination === "Not Detected").length,
+        detected: determinations.filter(d => d.determination === "Detected").length,
+        repeat: determinations.filter(d => d.determination === "Repeat").length,
+        inconclusive: determinations.filter(d => d.determination === "Inconclusive").length - determinations.filter(d => d.sample_id === "neg" || d.sample_id === "pc").length,
+        invalid: determinations.filter(d => d.determination === "Invalid").length,
+        control: determinations.filter(d => d.sample_id === "neg" || d.sample_id === "pc").length
+    }
 
     return (
         <GraphContextProvider>
@@ -29,6 +38,17 @@ export default () => {
                     <Card>
                         <ContextGraphOptions />
                         <SampleGraph sample={determination} />
+                    </Card>
+                    <Card style={{marginTop: 15}}>
+                        <h1>Statistics</h1>
+                        <Row>
+                            <Statistic title="Not Detected" value={statistics.notDetected} />
+                            <Statistic title="Detected" value={statistics.detected} style={{ margin: '0 20px' }} />
+                            <Statistic title="Repeat" value={statistics.repeat} />
+                            <Statistic title="Inconclusive" value={statistics.inconclusive} style={{ margin: '0 20px' }}/>
+                            <Statistic title="Invalid" value={statistics.invalid}/>
+                            <Statistic title="Controls" value={statistics.control} style={{ margin: '0 20px' }}/>
+                        </Row>
                     </Card>
                 </Col>
             </Row>
