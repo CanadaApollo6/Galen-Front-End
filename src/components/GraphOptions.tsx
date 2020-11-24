70e3ae577b6dd5ff461dd35e9c90060e852e97fa
CanadaApollo6 <riel.stamand@gmail.com>
2020-11-24 15:41:59 -0500

    Comment out scale graph option

--- src/components/GraphOptions.tsx
+++ src/components/GraphOptions.tsx
@@ -2,27 +2,27 @@ import React from 'react'
 import { Row, Col, Form } from 'antd'
 import { ContextTargetSelect } from './Inputs/TargetSelect'
 import { ContextGraphTypeSelect } from './Inputs/GraphTypeSelect'
-import { ContextScaleSelect } from './Inputs/ScaleSelect'
+//import { ContextScaleSelect } from './Inputs/ScaleSelect'
 
 export const ContextGraphOptions: React.FC = () =>
     <Form>
         <Row gutter={15}>
-            <Col span={8}>
+            <Col span={12}>
                 <Form.Item label='Target'>
                     <ContextTargetSelect />
                 </Form.Item>
             </Col>
 
-            <Col span={8}>
+            <Col span={12}>
                 <Form.Item label='Type'>
                     <ContextGraphTypeSelect />
                 </Form.Item>
             </Col>
 
-            <Col span={8}>
+            {/* <Col span={8}>
                 <Form.Item label='Scale'>
                     <ContextScaleSelect />
                 </Form.Item>
-            </Col>
+            </Col> */}
         </Row>
     </Form>
