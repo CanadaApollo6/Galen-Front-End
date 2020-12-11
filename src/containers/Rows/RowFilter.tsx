1cd117e7e803562b5ff46376463c6cf5f6b88b91
CanadaApollo6 <riel.stamand@gmail.com>
2020-12-11 14:08:04 -0500

    Formatting changes

--- src/containers/Rows/RowFilter.tsx
+++ src/containers/Rows/RowFilter.tsx
@@ -1,12 +1,12 @@
-import React, { useContext } from 'react'
-import { Row, Col, Form, Select, Badge } from 'antd'
-import { RowsContext } from './RowsContext'
-import PlateProgress from '../../components/PlateProgress'
+import React, { useContext } from "react";
+import { Row, Col, Form, Select, Badge } from "antd";
+import { RowsContext } from "./RowsContext";
+import PlateProgress from "../../components/PlateProgress";
 
 const RowFilter = () => {
     const { rows, row, setRow, showEvaluated, setShowEvaluated } = useContext(
         RowsContext
-    )
+    );
 
     return (
         <Form>
@@ -15,15 +15,17 @@ const RowFilter = () => {
                     <Form.Item label="Display Row">
                         <Select
                             value={row}
-                            onChange={(v) => setRow(v.toString())}
+                            onChange={(v) => {
+                                setRow(v.toString());
+                            }}
                             placeholder="Select Row..."
                         >
                             {rows.map(([r, n]) => (
                                 <Select.Option value={r}>
-                                    <div style={{ float: 'left' }}>
+                                    <div style={{ float: "left" }}>
                                         <span>{r}</span>
                                     </div>
-                                    <div style={{ float: 'right' }}>
+                                    <div style={{ float: "right" }}>
                                         <Badge count={n} />
                                     </div>
                                 </Select.Option>
@@ -49,7 +51,7 @@ const RowFilter = () => {
                 </Col>
             </Row>
         </Form>
-    )
-}
+    );
+};
 
-export default RowFilter
+export default RowFilter;
