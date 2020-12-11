0f6f6f5040a6ce3d2e2b2c0e13c888af611c3292
CanadaApollo6 <riel.stamand@gmail.com>
2020-12-11 14:12:50 -0500

    Updating AIs to resolve GD2 issue using CT values

--- src/services/S_GeneDetector.ts
+++ src/services/S_GeneDetector.ts
@@ -1,37 +1,43 @@
-import * as tf from '@tensorflow/tfjs'
-import { ExportSample } from '../types'
-import SGeneAmplfication from '../ai/SGeneAmplfication'
+import * as tf from "@tensorflow/tfjs";
+import { ExportSample } from "../types";
+import SGeneAmplfication from "../ai/SGeneAmplfication";
+import { findIntersects } from "./CTService";
 
-const normalize = (rns: number[]) => rns.filter((r, i) => i > 9)
+const normalize = (rns: number[]) => rns.filter((r, i) => i > 9);
 
 const getRank = (
     prediction: tf.Tensor<tf.Rank> | tf.Tensor<tf.Rank>[]
 ): tf.Tensor<tf.Rank> | undefined =>
-    !Array.isArray(prediction) ? prediction : undefined
+    !Array.isArray(prediction) ? prediction : undefined;
 
 export default async ({
     rns: { s_gene_delta },
 }: ExportSample): Promise<[boolean, number]> => {
-    const model = await SGeneAmplfication.getModel() as tf.LayersModel
-    const normalized = [normalize(s_gene_delta)]
+    const model = (await SGeneAmplfication.getModel()) as tf.LayersModel;
+    const normalized = [normalize(s_gene_delta)];
     const prediction = getRank(
         model.predict(tf.tensor(normalized).expandDims(0))
-    )
+    );
+    const guideLine = Array(40).fill(50000);
+    const ctValue = findIntersects(guideLine, s_gene_delta);
+    if (ctValue[0].x === "Not Found") {
+        return [false, 1];
+    }
 
     if (prediction) {
-        const array = await prediction.array()
+        const array = await prediction.array();
 
         if (Array.isArray(array)) {
-            const ai_value = array.flat(6)[0]
-            const confidence = Math.abs(ai_value - 0.5) * 2
+            const ai_value = array.flat(6)[0];
+            const confidence = Math.abs(ai_value - 0.5) * 2;
 
             if (confidence < 0.99) {
-                return [true, confidence]
+                return [true, confidence];
             }
 
-            return [ai_value < 0.5 ? false : true, confidence]
+            return [ai_value < 0.5 ? false : true, confidence];
         }
     }
 
-    return [true, 0]
-}
+    return [true, 0];
+};
